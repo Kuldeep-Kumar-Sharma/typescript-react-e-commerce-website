77bb7081d0c4adc28c922f7d0e38cfa458db5109
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-02-08 23:44:53 +0530

    added initial header

---
+++ src/components/containers/Layout/Header/Header.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+//for small styles
+const navBarStyles = {
+  backgroundColor: "#fff",
+  backgroundImage: "linear-gradient(0deg, #D2D2D2 0%, #97D9E1 100%);",
+  boxShadow: "0 0 6px 0 rgba(0, 0, 0, 0.3)",
+};
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <Navbar
+        bg="light"
+        variant="light"
+        expand="lg"
+        sticky="top"
+        style={navBarStyles}
+      >
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="/loading">Loading</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="nav-scroller bg-white shadow-sm">
+        <Container>
+          <nav className="nav nav-underline"></nav>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
