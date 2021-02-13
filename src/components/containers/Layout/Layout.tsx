f746494e02ca8edeb6b1ee8e42b42a096e49820d
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-02-13 23:51:23 +0530

    updated the Carousel in Gallery

--- src/components/containers/Layout/Layout.tsx
+++ src/components/containers/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header/Header";
+import Gallery from "../../../pages/Products/Gallery/Gallery";
 
 const Layout = () => {
   return (
     <div>
       <Header />
+      <Gallery />
     </div>
   );
 };
---
+++ src/pages/Products/Gallery/Gallery.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+const Gallery: React.FC = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1574758187370-1c8d72ee24d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1562070354-930df66ead59?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1587226516191-47ad119ba2fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Gallery;
