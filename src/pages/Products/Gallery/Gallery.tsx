cd8b2591bc0abfa66f0dc30c746221c219dd1876
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-02-16 23:46:52 +0530

    gallery formatted

--- src/pages/Products/Gallery/Gallery.tsx
+++ src/pages/Products/Gallery/Gallery.tsx
@@ -1,51 +1,55 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 
-const divStyles = {
-  height: "50px",
-};
+//sample array to recive data
+const data = [
+  {
+    imageSource:
+      "https://images.unsplash.com/photo-1574758187370-1c8d72ee24d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    imageAltText: "Sample Alt text",
+    productName: "Dummy One",
+    productDiscription: "Dummy One Dummy Discription",
+    productID: 123,
+  },
 
+  {
+    imageSource:
+      "https://images.unsplash.com/photo-1587226516191-47ad119ba2fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    imageAltText: "Sample Alt text",
+    productName: "Dummy One",
+    productDiscription: "Dummy One Dummy Discription",
+    productID: 123,
+  },
+
+  {
+    imageSource:
+      "https://images.unsplash.com/photo-1562070354-930df66ead59?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80",
+    imageAltText: "Sample Alt text",
+    productName: "Dummy One",
+    productDiscription: "Dummy One Dummy Discription",
+    productID: 123,
+  },
+];
 const Gallery: React.FC = () => {
   return (
-    <div style={divStyles}>
+    <div>
       <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.unsplash.com/photo-1574758187370-1c8d72ee24d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.unsplash.com/photo-1562070354-930df66ead59?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.unsplash.com/photo-1587226516191-47ad119ba2fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {data.map((product) => {
+          return (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                height="550px"
+                src={product.imageSource}
+                alt={product.imageAltText}
+              />
+              <Carousel.Caption>
+                <h3>slide label</h3>
+                <p>{product.productDiscription}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
       </Carousel>
     </div>
   );
