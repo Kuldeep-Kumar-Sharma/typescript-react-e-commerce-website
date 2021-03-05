03807c3f0d475fee2d651d228ce561d54f5f986d
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-03-05 23:12:11 +0530

    updated cart reducer

---
+++ src/Models/Cart.ts
@@ -0,0 +1,3 @@
+export interface Cart {
+  products: Array<string>;
+}
--- src/Store/cartReducer.ts
+++ src/Store/cartReducer.ts
@@ -1,5 +1,37 @@
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CartActionTypes,
+} from "../Actions/types";
+import { Cart } from "../Models/Cart";
+
 //use normal redux
 
-const cartReducer = {};
+const initialState: Cart = {
+  products: [],
+};
+
+const cartReducer = (
+  state: Cart = initialState,
+  action: CartActionTypes
+): Cart => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        products: state.products.concat(action.product_id),
+      };
+    case REMOVE_FROM_CART:
+      const updatedProducts: string[] = state.products.filter(
+        (id) => id !== action.product_id
+      );
+      return {
+        ...state,
+        products: updatedProducts,
+      };
+    default:
+      return state;
+  }
+};
 
 export default cartReducer;
