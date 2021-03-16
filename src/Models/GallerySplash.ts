fb256c27b5311dedcb7e48251e35aed7d443226d
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-03-16 23:48:35 +0530

    updated the code

---
+++ src/Models/GallerySplash.ts
@@ -0,0 +1,7 @@
+export interface GallerySplash {
+  imageSource: string;
+  imageAltText: string;
+  splashName: string;
+  splashDiscription: string;
+  searchID: string;
+}
--- src/components/containers/Layout/Layout.tsx
+++ src/components/containers/Layout/Layout.tsx
@@ -5,73 +5,9 @@ import Gallery from "../../../Pages/Products/Gallery";
 import CartCo from "../../../Pages/Products/Cart";
 import Profile from "../../../Pages/User/Profile";
 import Footer from "./Footer/Footer";
-import { ProductCardProps } from "../../../Models/ProductCard";
 
-import { User } from "../../../Models/User";
-import { Address } from "../../../Models/Address";
-import { Product } from "../../../Models/Product";
-import { Order } from "../../../Models/Order";
 import { Cart } from "../../../Models/Cart";
-import { SearchProps, filterItem } from "../../../Models/Search";
 import { Route, Switch } from "react-router-dom";
-
-const PC: ProductCardProps = {
-  productId: "SN1025GN",
-  name: "Blouse",
-  price: 250,
-  sale: true,
-  discount: 10,
-  rating: 5,
-  discription:
-    "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
-  productImage:
-    "http://bestjquery.com/tutorial/product-grid/demo9/images/img-1.jpg",
-  productImage2:
-    "http://bestjquery.com/tutorial/product-grid/demo9/images/img-2.jpg",
-};
-
-const FI: filterItem = {
-  eventKey: "price",
-  name: "NAME",
-};
-
-const SP: SearchProps = {
-  term: "Blouse",
-  productCards: [PC],
-  filters: [FI],
-};
-const custAddress: Address = {
-  hno: "38",
-  address: "dummy road",
-  city: "dummy land",
-  district: "dummy city",
-  pin: 142365,
-};
-const PCs: Product = {
-  productId: "SN1025GN",
-  name: "Blouse",
-  price: 250,
-  sale: true,
-  discount: 10,
-  discription:
-    "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
-  productImage:
-    "http://bestjquery.com/tutorial/product-grid/demo9/images/img-1.jpg",
-};
-const order: Order = {
-  date: "20/03/2021",
-  time: "05:14:00",
-  product: PC,
-};
-const user: User = {
-  name: "abcCustomer",
-  phone: "9781425369",
-  email: "abcCustomer@gmail.com",
-  profileImage:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog",
-  address: [custAddress],
-  orders: [order],
-};
 
 const Layout: React.FC = () => {
   const cart: readonly string[] = useSelector(
