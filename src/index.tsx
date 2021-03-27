dd564b1d3a9aae36f72215b9a4fde227ce5ea5c6
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-03-27 23:30:04 +0530

    updated the code

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware, Store } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import { Cart } from "./Models/Cart";
 import { CartActionTypes, DispatchType } from "./Actions/types";
 import "bootstrap/dist/css/bootstrap.css";
 import cartReducer from "./Store/cartReducer";
+
+//for dev tools
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 // for now only cart reducer
 const store: Store<Cart, CartActionTypes> & {
