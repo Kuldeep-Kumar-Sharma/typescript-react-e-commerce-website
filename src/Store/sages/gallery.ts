869c94079eb1b61670ada323b3445190edb9e576
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-04-08 23:33:36 +0530

    updated

--- src/Store/sages/gallery.ts
+++ src/Store/sages/gallery.ts
@@ -3,18 +3,9 @@ import axios from "../../axios-instance";
 import { GallerySplash } from "../../Models/GallerySplash";
 import { Creators } from "../actions/galleryActionCreators";
 
-// get call to bring splash screens
-function getCall() {
-  try {
-    return axios.get<GallerySplash[]>("/splashes");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 export function* initGallerySplashSaga(actions: Creators) {
   try {
-    const response: any = yield* call(getCall);
+    const response: any = yield* call(axios.get<GallerySplash[]>("/splashes"));
     yield put(actions.loadGallerySplashes(response));
   } catch (error) {
     console.log("--[initGallerySplashSaga]--");
