5424a2fc471940dee44900cb562d31501413f401
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-04-09 23:50:36 +0530

    checking the issue

--- src/Store/sages/gallery.ts
+++ src/Store/sages/gallery.ts
@@ -1,11 +1,26 @@
-import { put, call } from "typed-redux-saga";
+import { put } from "typed-redux-saga";
 import axios from "../../axios-instance";
 import { GallerySplash } from "../../Models/GallerySplash";
 import { Creators } from "../actions/galleryActionCreators";
 
+// get call to bring splash screens
+function getCall(): any {
+  const values: any[] = [];
+  try {
+    axios.get<GallerySplash[]>("/splashes").then((response) => {
+      for (const value of response.data) {
+        values.push(value);
+      }
+      return values;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export function* initGallerySplashSaga(actions: Creators) {
   try {
-    const response: any = yield* call(axios.get<GallerySplash[]>("/splashes"));
+    const response: any = yield* getCall();
     yield put(actions.loadGallerySplashes(response));
   } catch (error) {
     console.log("--[initGallerySplashSaga]--");
--- src/Store/sages/index.js
+++ src/Store/sages/index.js
@@ -4,7 +4,6 @@ import { initGallerySplashSaga } from "./gallery";
 import * as galleryTypes from "../actions/galleryTypes";
 
 export function* watchGallery() {
-  yield takeLatest([
-    takeEvery(galleryTypes.LOAD_SPLASH_IMAGES, initGallerySplashSaga),
-  ]);
+  yield* takeEvery(galleryTypes.LOAD_SPLASH_IMAGES, initGallerySplashSaga),
+  
 }
