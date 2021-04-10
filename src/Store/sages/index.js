84de18b22f28d2cdec88cc55c41889d509b5d254
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-04-10 23:02:52 +0530

    checked saga

--- src/Store/sages/index.js
+++ src/Store/sages/index.js
@@ -4,6 +4,5 @@ import { initGallerySplashSaga } from "./gallery";
 import * as galleryTypes from "../actions/galleryTypes";
 
 export function* watchGallery() {
-  yield* takeEvery(galleryTypes.LOAD_SPLASH_IMAGES, initGallerySplashSaga),
-  
+  yield* takeEvery(galleryTypes.LOAD_SPLASH_IMAGES, initGallerySplashSaga);
 }
