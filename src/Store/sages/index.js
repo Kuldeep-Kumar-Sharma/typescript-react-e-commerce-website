1d28a981ed5b70e968eabaae9a881f58b6b0d5e0
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-04-12 23:47:05 +0530

    removed sages

--- src/Store/sages/index.js
+++
@@ -1,8 +0,0 @@
-import { takeEvery, takeLatest } from "redux-saga/effects";
-import { initGallerySplashSaga } from "./gallery";
-
-import * as galleryTypes from "../actions/galleryTypes";
-
-export function* watchGallery() {
-  yield* takeEvery(galleryTypes.LOAD_SPLASH_IMAGES, initGallerySplashSaga);
-}
