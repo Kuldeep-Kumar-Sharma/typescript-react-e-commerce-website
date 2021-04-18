90bb659568dbe30b58e15aac35a13b7d56bb40ac
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-04-18 23:52:49 +0530

    adding the Thunk

--- src/Store/actions/galleryActionCreators.ts
+++ src/Store/actions/galleryActionCreators.ts
@@ -1,11 +1,26 @@
 import { LOAD_SPLASH_IMAGES, GalleryActionTypes } from "./galleryTypes";
 import { GallerySplash } from "../../Models/GallerySplash";
+import { Dispatch } from "redux";
+import axios from "../../axios-instance";
 
-export class Creators {
-  loadGallerySplashes(gallerySplashes: GallerySplash[]): GalleryActionTypes {
-    return {
-      type: LOAD_SPLASH_IMAGES,
-      gallerySplashes: gallerySplashes,
-    };
-  }
-}
+export const setGallerySplashes = (
+  gallerySplashes: GallerySplash[]
+): GalleryActionTypes => {
+  return {
+    type: LOAD_SPLASH_IMAGES,
+    gallerySplashes: gallerySplashes,
+  };
+};
+
+export const loadGallerySplashes = (action: GalleryActionTypes) => {
+  return (dispatch: Dispatch) => {
+    axios
+      .get("https://react-my-burger.firebaseio.com/ingredients.json")
+      .then((response) => {
+        dispatch(setGallerySplashes(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
