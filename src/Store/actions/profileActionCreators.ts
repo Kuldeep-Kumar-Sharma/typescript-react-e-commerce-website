b65e2d7b689388dd1f3fcae8d905f3b4254a8a36
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-05-02 23:53:43 +0530

    profile reducer

--- src/Store/actions/profileActionCreators.ts
+++ src/Store/actions/profileActionCreators.ts
@@ -2,14 +2,14 @@ import { EDIT_PROFILE, RETRIVE_PROFILE } from "./profileTypes";
 
 export function editProfile(ProductId: string): CartActionTypes {
   return {
-    type: ADD_TO_CART,
+    type: EDIT_PROFILE,
     product_id: ProductId,
   };
 }
 
 export function removeromCart(ProductId: string): CartActionTypes {
   return {
-    type: REMOVE_FROM_CART,
+    type: RETRIVE_PROFILE,
     product_id: ProductId,
   };
 }
